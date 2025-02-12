3789bdea637a5735ba43389b0c39681d1bd6955d
alan <alan@HPx360>
2025-02-12 16:41:13 -0600

    * Added capabiilty for time-based units * Some re-formatting and cleanup

--- gpx-interval.py
+++ gpx-interval.py
@@ -1,7 +1,7 @@
-# gpx-interval.py - find fastest interval(s) of given length from GPX file
+# gpx-interval.py - find fastest interval(s) of given length or time from GPX file
 #
 # usage: python gpx-interval.py [-2d] [-interval NN.<unit>] <file>.gpx
-#   where <unit> is one of ft, yd, mi, m, km
+#   where <unit> is one of ft, yd, mi, m, km, sec, min, hr
 
 # Adapted from https://www.wouternieuwerth.nl/how-to-find-the-fastest-section-within-a-gpx-file-with-python-jupyter-notebooks/
 
@@ -19,29 +19,42 @@ from timezonefinder import TimezoneFinder
 
 tzf = TimezoneFinder() 
 
-# Target interval lengths - defaults to 100m, 1 mile, 5 miles
+# Default target interval lengths if not specified
 default_intervals = ['100m', '1mi', '5mi']
 # If true, ignore altitude data to avoid skewing distances. For e.g. sailing
 use_2d = False
 
+#################################################################################
 # Helper class to represent an interval specified with various units
 class Interval:
-  # table to convert from meters to unit
-  conv_table = {'ft' : 3.28084, 
-                'yd' : 1.093613,
-                'mi' : 0.0006213712,
-                'm'  : 1.0,
-                'km' : .001};
+  # table to classify and normalize units
+  # - factor converts distance to meters and time to seconds
+  unit_table = {'ft' : { 'type' : 'distance', 'factor' : 3.28084 }, 
+                'yd' : { 'type' : 'distance', 'factor' : 1.093613 },
+                'mi' : { 'type' : 'distance', 'factor' : 0.0006213712 },
+                'm'  : { 'type' : 'distance', 'factor' : 1.0 },
+                'km' : { 'type' : 'distance', 'factor' : .001 }, 
+                'sec': { 'type' : 'time',     'factor' : 1.0 }, 
+                'min': { 'type' : 'time',     'factor' : 0.0166666667 },
+                'hr' : { 'type' : 'time',     'factor' : 0.0002777778 } }
+  # construct an interval from a string
   def __init__(self, init_str):
-    m = re.match('([0-9]+)(ft|yd|mi|m|km)', init_str)
-    if not m:
-       raise ValueError('interval specification should be NNN.[ft|yd|mi|m|km]')
-    self.value = int(m[1])
-    self.unit = m[2]   
+    for unit in Interval.unit_table.keys():
+       m = re.fullmatch('([0-9]+)' + unit, init_str)
+       if (m):
+          self.value = int(m[1])
+          self.unit = unit
+          self.is_distance = Interval.unit_table[unit]['type'] == 'distance'
+          return
+    valid_units = '|'.join(Interval.unit_table.keys())
+    raise ValueError('interval specification should be NNN.[' + valid_units + ']')
+  # normalize interval: distance to meters, time to seconds
+  def normalize(self):
+    return self.value / Interval.unit_table[self.unit]['factor']
   def __str__(self):
-    return str(self.value) + ' ' + self.unit
-  def to_meters(self):
-    return self.value / self.conv_table[self.unit]
+    normalized_units = 'm' if self.is_distance else 'sec'
+    return f"{self.value} {self.unit} ({self.normalize():.1f} {normalized_units})"
+
 
 #################################################################################
 def main():
@@ -76,14 +89,14 @@ def main():
     find_best_interval(interval)
 
 #################################################################################
-# Parse the file and create a table from the data
+# Parse the file and set up a pandas table that will be used for processing
 def read_gpx(filename):
   # Read all the points from all tracks in the GPX file
   points = []
   for file in [filename]:
     gpx_file = open(filename, 'r')
     gpx = gpxpy.parse(gpx_file)
-    print("parsed file: ", filename)
+    print(f"parsed file: {filename}")
 
     for segment in gpx.tracks[0].segments: # all segments
       data = segment.points
@@ -93,7 +106,7 @@ def read_gpx(filename):
                        'alt' : point.elevation, 
                        'timestamp' : point.time})
         #print(points)
-  print("processed ", len(points), " points")
+  print(f"processed {len(points)} points")
 
   if len(points) == 0:
     print("empty track")
@@ -109,8 +122,8 @@ def read_gpx(filename):
   df.ffill(inplace=True)
   df.bfill(inplace=True)
 
-  # Create column with values that are 'shifted' one forwards, so we can 
-  # create calculations for differences.
+  # Create columns with position values shifted one point forwards, to enable
+  # caclulating deltas (below)
   df['lon-start'] = df['lon']
   df['lon-start'] = np.roll(df['lon-start'], 1)
   df['lat-start'] = df['lat']
@@ -120,27 +133,27 @@ def read_gpx(filename):
 
   df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True)
 
-  # Add columns with time and distance deltas
+  # Calculate point-to-point time and distance deltas into new columns
   df['elapsed_time'] = df.apply(
       lambda x: (x['timestamp'] - df['timestamp'].iloc[0]).total_seconds(), 
       axis=1)
-  df['distance_dis_2d'] = df.apply(
+  df['delta_2d'] = df.apply(
       lambda x: distance.distance((x['lat-start'], x['lon-start']), 
                                   (x['lat'], x['lon'])).m, 
       axis = 1)
-  df['alt_dif'] = df.apply(
+  df['delta_alt'] = df.apply(
       lambda x: x['alt-start'] - x['alt'], 
       axis=1)
-  df['distance_dis_3d'] = df.apply(
-      lambda x: sqrt(x['distance_dis_2d']**2 + (x['alt_dif'])**2), 
+  df['delta_3d'] = df.apply(
+      lambda x: sqrt(x['delta_2d']**2 + (x['delta_alt'])**2), 
       axis=1)
 
-  df.at[0, 'distance_dis_2d'] = 0
-  df.at[0, 'distance_dis_3d'] = 0
-  df.at[0, 'alt_dif'] = 0
+  df.at[0, 'delta_2d'] = 0
+  df.at[0, 'delta_3d'] = 0
+  df.at[0, 'delta_alt'] = 0
 
-  # Use 2d or 3d distance to calculate cumulative distance at each point
-  distance_tag = 'distance_dis_2d' if use_2d else 'distance_dis_3d'
+  # Accumulate 2d or 3d deltas to calculate cumulative distance at each point
+  distance_tag = 'delta_2d' if use_2d else 'delta_3d'
   df['elapsed_distance'] = df[distance_tag].cumsum()
 
   # Create a column with timestamp converted to local time, for reporting
@@ -151,46 +164,49 @@ def read_gpx(filename):
 
   # Report some info from the file
   filedate = df['ts_local'].iloc[0]
+  dtype = '2d' if use_2d else '3d'
   print(f"date: {filedate.strftime('%Y-%m-%d %H:%M:%S%p (UTC%z, %Z)')}")
-  print(f"total distance: {df['elapsed_distance'].iloc[-1]:.1f} m", end=' ')
-  print(f"({'2d' if use_2d else '3d'})")
-  print(f"total time: {df['elapsed_time'].iloc[-1]:.1f} sec")
+  print(f"track distance: ({dtype}): {df['elapsed_distance'].iloc[-1]:.1f} m")
+  print(f"track time: {df['elapsed_time'].iloc[-1]:.1f} sec")
 
 #################################################################################
-# Find fastest interval equal to or exceeding the given target
+# Find fastest interval equal to or exceeding the given target. Interval can be
+# specified as distance (eg. 1 mi) or time (eg. 1 min)
 def find_best_interval(interval):
-  target_interval = interval.to_meters()
-  print(f"\ntarget interval: {interval} ({target_interval:.1f} m):")
+  target_interval = interval.normalize()
 
   # Skip if the total distance of the track is smaller then the target
-  if df['elapsed_distance'].iloc[-1] < target_interval:
-      print("no intervals found (track too short)")
-      return
+  print(f"\ntarget interval: {interval}:")
+  key = 'elapsed_distance' if interval.is_distance else 'elapsed_time'
+  if df[key].iloc[-1] < target_interval:
+    print("no intervals found (track too short)")
+    return
 
   # Iterate over points in the track. From each point (i), find the first point (j)
-  # with least as much delta distance as the target interval. Record the interval
-  # with the best speed.
+  # with least as much delta distance or time as the target interval. Record the 
+  # interval with the best speed.
   best_interval = {'start': None, 'end': None, 'speed': None }
   j = 1
   for i in range(len(df.index)-2):
-      j = max(j, i+1)
-      while j < len(df.index):
-          dist = df['elapsed_distance'].iloc[j] - df['elapsed_distance'].iloc[i]
-          if dist < target_interval:
-              j = j + 1
-          else:
-              elapsed = df['elapsed_time'].iloc[j] - df['elapsed_time'].iloc[i]
-              speed = dist / elapsed
-              if best_interval['start'] == None or speed > best_interval['speed']:
-                  best_interval.update({'start': i, 'end': j, 'speed': speed})
-              # note: don't advance j, since [i+1,j] could be a candidate
-              break
+    j = max(j, i+1)
+    while j < len(df.index):
+      dist = df['elapsed_distance'].iloc[j] - df['elapsed_distance'].iloc[i]
+      elapsed = df['elapsed_time'].iloc[j] - df['elapsed_time'].iloc[i]
+      span = dist if interval.is_distance else elapsed
+      if span < target_interval:
+        j = j + 1
+      else:
+        speed = dist / elapsed
+        if best_interval['start'] == None or speed > best_interval['speed']:
+          best_interval.update({'start': i, 'end': j, 'speed': speed})
+        # note: don't advance j, since [i+1,j] could also be a candidate
+        break
 
-  # This should not happen; if track is long enough there should be at least 
+  # This should not happen; with a long-enough track there must be at least 
   # one interval
   if not best_interval['start']:
-      print("no interval found")
-      return
+     print("no interval found")
+     return
 
   # Report results from the winning interval 
   [start_idx, end_idx] = [best_interval['start'], best_interval['end']]
@@ -199,15 +215,16 @@ def find_best_interval(interval):
   distance = end_point['elapsed_distance'] - start_point['elapsed_distance']
   elapsed = end_point['elapsed_time'] - start_point['elapsed_time']
 
-  print(f"start={start_point['ts_local'].strftime('%H:%M:%S')}", end=' ')
+  print(f"   start: {start_point['ts_local'].strftime('%H:%M:%S')}", end=' ')
   print(f"(T+{time.strftime('%H:%M:%S', time.gmtime(start_point['elapsed_time']))})", end=' ')
   print(f"(index={start_idx})")
-  print(f"end={end_point['ts_local'].strftime('%H:%M:%S')}", end=' ')
+  print(f"   end: {end_point['ts_local'].strftime('%H:%M:%S')}", end=' ')
   print(f"(T+{time.strftime('%H:%M:%S', time.gmtime(end_point['elapsed_time']))})", end=' ')
   print(f"(index={end_idx})")
-  print(f"time={elapsed:.2f} sec")
-  print(f"dist={distance:.1f} m ({distance*0.0006213712:.3f} mi)" )
-  print(f"speed={speed:.2f} m/s ({speed*0.0006213712*3600:.2f} mph)")
+  print(f"   time: {elapsed:.2f} sec")
+  print(f"   distance: {distance:.1f} m ({distance*0.0006213712:.3f} mi)" )
+  print(f"   speed: {speed:.2f} m/s ({speed*0.0006213712*3600:.2f} mph)")
 
+#################################################################################
 if __name__ == "__main__":
     main()
